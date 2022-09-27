a8909ec32a0fefec101c59a650895fe6a8513869
Rohit Paul Gerard Nagarajan <114315272+rohitpaultech@users.noreply.github.com>
2022-09-27 00:44:50 -0400

    Add files via upload

---
+++ Assignment1 - Human Resource Officer/src/model/HumanResources.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Rohit Paul G
+ */
+public class HumanResources {
+    
+    private String name;
+    private String empId;
+    private String age;
+    private String gendr;
+    private String strtDate;
+    private String lvl;
+    private String tmInfo;
+    private String posTitle;
+    private String cellPhn;
+    private String emailAdd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGendr() {
+        return gendr;
+    }
+
+    public void setGendr(String gendr) {
+        this.gendr = gendr;
+    }
+
+    public String getStrtDate() {
+        return strtDate;
+    }
+
+    public void setStrtDate(String strtDate) {
+        this.strtDate = strtDate;
+    }
+
+    public String getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(String lvl) {
+        this.lvl = lvl;
+    }
+
+    public String getTmInfo() {
+        return tmInfo;
+    }
+
+    public void setTmInfo(String tmInfo) {
+        this.tmInfo = tmInfo;
+    }
+
+    public String getPosTitle() {
+        return posTitle;
+    }
+
+    public void setPosTitle(String posTitle) {
+        this.posTitle = posTitle;
+    }
+
+    public String getCellPhn() {
+        return cellPhn;
+    }
+
+    public void setCellPhn(String cellPhn) {
+        this.cellPhn = cellPhn;
+    }
+
+    public String getEmailAdd() {
+        return emailAdd;
+    }
+
+    public void setEmailAdd(String emailAdd) {
+        this.emailAdd = emailAdd;
+    }
+    
+    //as per the error shown in jTable first column has printed irrelevant value plus our value from
+    //create employee screen. In order to workaround that issue, we do below:
+    
+    @Override
+    public String toString(){
+        
+        return name;
+        
+    }
+    
+    
+}
