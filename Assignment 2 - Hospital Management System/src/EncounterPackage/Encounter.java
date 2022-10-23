4e39d8de3e0fd1071f0cfc3532f165f456d9a07c
Rohit Paul Gerard Nagarajan <114315272+rohitpaultech@users.noreply.github.com>
2022-10-23 00:58:03 -0400

    Add files via upload

---
+++ Assignment 2 - Hospital Management System/src/EncounterPackage/Encounter.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EncounterPackage;
+
+import PersonPackage.Person;
+
+/**
+ *
+ * @author Rohit Paul G
+ */
+public class Encounter {
+        
+    private Person person;
+    private String patientId;
+    private String listOfDoctors;
+    private String bloodType;
+    private String admitDate;
+    private String symptoms;
+
+    public Encounter(Person person, String patientId, String listOfDoctors, String bloodType, String admitDate, String symptoms) {
+        this.person = person;
+        this.patientId = patientId;
+        this.listOfDoctors = listOfDoctors;
+        this.bloodType = bloodType;
+        this.admitDate = admitDate;
+        this.symptoms = symptoms;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getListOfDoctors() {
+        return listOfDoctors;
+    }
+
+    public void setListOfDoctors(String listOfDoctors) {
+        this.listOfDoctors = listOfDoctors;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getAdmitDate() {
+        return admitDate;
+    }
+
+    public void setAdmitDate(String admitDate) {
+        this.admitDate = admitDate;
+    }
+
+    public String getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(String symptoms) {
+        this.symptoms = symptoms;
+    }
+    
+    
+    
+}
---
+++ Assignment 2 - Hospital Management System/src/PersonPackage/PersonDirectory.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PersonPackage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rohit Paul G
+ */
+public class PersonDirectory {
+    
+    private ArrayList<Person> personlist;
+
+    public PersonDirectory() {
+        personlist = new ArrayList<>();   
+    }
+
+    public ArrayList<Person> getPersonlist() {
+        return personlist;
+    }
+
+    public void setPersonlist(ArrayList<Person> personlist) {
+        this.personlist = personlist;
+    }
+    
+    public Person addNewPerson(String fName, String lName, int age, String dateOfBirth, double height, float weight, String mobilePhn, String roles, String hospital, String city, String community, String userName, String passWord, String houseAddress){
+        
+        Person np = new Person(fName, lName, age, dateOfBirth, height, weight, mobilePhn, roles, hospital, city, community, userName, passWord, houseAddress);
+        personlist.add(np);
+        return np;
+    }
+    
+    
+    
+}
